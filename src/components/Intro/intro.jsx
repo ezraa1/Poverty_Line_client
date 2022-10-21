7d9a60dadf52d59fdb8923da201a83f793ff4b29
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-10-21 17:03:44 +0300

    adding an intro to homepage

---
+++ src/components/Intro/intro.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const intro = () =>{
+    return(
+        <div className="intro">
+            <div className="intro-left">
+                <div className="intro-name">
+                <span>Hey! What are looking at?</span>
+                <span>Luckily am not agree today</span>
+                <span>In the case of our random word generator, a random word is a word that is randomly chosen from the database of words we have in our generator.</span>
+                </div>  
+                <button className="button intro-button">Click here</button> 
+            </div>
+            <div className="intro-right"></div>
+        </div>
+
+    )
+}
+export default intro;
