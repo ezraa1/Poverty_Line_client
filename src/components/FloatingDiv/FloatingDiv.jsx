4c2d70bc2d7dcf75d92ad22d558ff991c5e30c9a
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-10-21 19:11:08 +0300

    styling home page

---
+++ src/components/FloatingDiv/FloatingDiv.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FloatingDiv = () =>{
+    return(
+        <div className="floatingdiv">
+        <img src="" alt="" />
+        <span>
+            charity
+            <br />
+            Organization  
+
+        </span>
+        </div>
+    )
+}
+
+export default FloatingDiv;
--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import './intro.css'
+import './intro.css';
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
 
 const intro = () =>{
     return(
@@ -11,8 +19,20 @@ const intro = () =>{
                 <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, cupiditate tempore? Aut quos pariatur, quasi ea officia accusantium ipsum? Aut quam ex itaque veritatis aspernatur delectus! Perspiciatis natus mollitia molestiae!</span>
                 </div>  
                 <button className="button intro-button">Click here</button> 
+        <div className="intro-icons">
+          <img src={LinkedIn} alt="" />
+          <img src={Instagram} alt="" />
+        </div>
             </div>
-            <div className="intro-right">right side</div>
+            <div className="intro-right">
+            <img src={Vector1} alt="" />
+          <img src={Vector2} alt="" />
+          <img src={boy} alt="" />
+          <div>
+            <FloatingDiv />
+          </div>
+
+            </div>
         </div>
 
     )
