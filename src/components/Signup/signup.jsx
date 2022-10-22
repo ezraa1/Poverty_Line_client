ef94212ff58cdbd130fdf13fb93b1ea375bd0d46
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-10-22 18:40:17 +0300

    created the basics coding

--- src/components/Signup/signup.jsx
+++ src/components/Signup/signup.jsx
@@ -13,6 +13,17 @@ const Signup = ( ) => {
             Login
           </button>
         </div>
+      <div className="form">
+      <form>
+        <input type="text" name="firstname" placeholder="Enter your firstname................" required   />
+        <input type="text" name="lastname" placeholder="Enter your lastname................" required   />
+        <input type="email" name="email" placeholder="Enter you email............" required  />
+        <input type="text" name="country" placeholder="Kenya..........." required  />
+        <input type="password" name="password" placeholder="Enter you password............" required   />
+        <input type="Confirm password" name="Confirm password" placeholder="Enter you password............" required   />
+        <input type="tel" name="phone" placeholder="+254 123 456 789........." required  />
+          </form>
+        </div>
         <div className="button">
         <button type="submit" className="btn btn-primary">
           Submit
