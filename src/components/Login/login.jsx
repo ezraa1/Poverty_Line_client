ff71687c695cd5f2f6474d69bfa8b9e2131effb1
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-10-23 17:40:14 +0300

    done with the styling part

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -5,6 +5,7 @@ import React, { useState } from "react"
 import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
+
 const Login = ( ) => {
   const [data, setData] =useState({
       fullname: "",
@@ -15,13 +16,13 @@ const Login = ( ) => {
     return(
         <div className="nav">
             <div className="bar">
-        <FaArrowLeft /><h3>Back to Homepage</h3>
+        <FaArrowLeft /><h3><i class="fa fa-arrow-left" aria-hidden="true"></i>Back to Homepage</h3>
             <h1>WELCOME BACK!</h1>
             <img src={IMG} alt="Logo Image" />
                 </div>
 
             <div className="form">
-            <form onSubmit>
+            <form onSubmit >
             <input type="text" name="fullname" placeholder="Enter Fullname................" required  />
             <input type="password" name="password" placeholder="Enter Password............" required />
             </form>
