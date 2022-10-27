0a8976c06a5c604866568e6bf0d3b3fcaec3b9b3
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-10-27 12:05:09 +0300

    new signup login features

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -3,7 +3,7 @@ import {FaArrowLeft} from "react-icons/fa"
 import IMG from "../../assets/hand.jpg"
 import React, { useState } from "react"
 import { NavLink } from "react-router-dom"
-
+import { useNavigate } from "react-router-dom"
 
 const Login = ( ) => {
   const [data, setData] =useState({
@@ -11,19 +11,84 @@ const Login = ( ) => {
       password: "",
     });
 
+    const [errors, setErrors] = useState(null);
+    const [success, setSuccess] = useState(null);
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+      setErrors(null);
+      const { name } = event.target;
+      const { value } = event.target;
+      setData({ ...data, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const response = await fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          },
+          body: JSON.stringify(data),
+        });
+        const user = await response.json();
+        if (response.ok) {
+          setSuccess("Logged in successfully!");
+          setData({ username: "", password: "" });
+          setTimeout(() => {
+            navigate("/home", { state: user });
+          }, 2000);
+        } else {
+          setErrors(user);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     return(
         <div className="nav">
+        <div className="arrow">
+        <FaArrowLeft />
+        </div>
             <div className="bar">
-        <FaArrowLeft /><h3><i class="fa fa-arrow-left" aria-hidden="true"></i>Back to Homepage</h3>
+            {errors !== null ? (
+              <div
+                style={{
+                  color: "orange",
+                  padding: "0.1rem",
+                  margin: "1rem 0",
+                }}
+              >
+                {errors.error}
+              </div>
+            ) : null}
+
+            {success !== null ? (
+              <div
+                style={{
+                  color: "green",
+                  padding: "0.1rem",
+                  margin: "1rem 0",
+                }}
+              >
+                {success}
+              </div>
+            ) : null}
+       <h3>Back to Homepage</h3>
             <h1>WELCOME BACK!</h1>
             <img src={IMG} alt="Logo Image" />
                 </div>
 
             <div className="form">
-            <form onSubmit >
-            <input type="text" name="fullname" placeholder="Enter Fullname................" required  />
-            <input type="password" name="password" placeholder="Enter Password............" required />
+            <form onSubmit= {handleSubmit} >
+            <input type="text" name="fullname" placeholder="Enter Fullname................" required
+            value={data.fullname}
+            onChange={handleChange}/>
+            <input type="password" name="password" placeholder="Enter Password............" required
+            value={data.password}
+            onChange={handleChange}/>
             </form>
         </div>
 
--- src/components/Signup/signup.jsx
+++ src/components/Signup/signup.jsx
@@ -1,19 +1,100 @@
-import React from "react"
+import React, {useState} from "react"
 import "./signup.css"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
+import IMG from "../../assets/hand.jpg"
 
 const Signup = ( ) => {
+    const [data, setData] = useState({
+      firstname: "",
+      lastname: "",
+      fullname: "",
+      email: "",
+      country: "",
+      phone: "",
+      password: "",
+      confirmpassword: "",
+    })
+
+    const [errors, setErrors] = useState(null);
+    const [success, setSuccess] = useState(null);
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    const handleChange = (event) => {
+      const { name } = event.target;
+      const { value } = event.target;
+      setData({ ...data, [name]: value });
+    };
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const response = await fetch("/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json;charset=utf-8",
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+          setUser(user);
+          setSuccess("Signup is successfull!");
+          setData({
+            firstname: "",
+      lastname: "",
+      email: "",
+      country: "",
+      phone: "",
+      password: "",
+      confirmpassword: "",
+          });
+
+          setTimeout(() => {
+            navigate("/footer", { state: user });
+          }, 2000);
+        } else {
+          setErrors(user);
+        }
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     return(
         <div className="log">
-        <div className="par">
-        <p>“Do not look the other way;
-        do not hesitate. Recognise
-        that the world is hungry for
-         action, not words. Act with courage and vision.”</p>
-        </div>
             <div className="sign">
+            {errors !== null ? (
+              <div
+                style={{
+                  color: "orange",
+                  padding: "0.1rem",
+                  margin: "1rem 0",
+                }}
+              >
+                <ul>
+                  {errors.errors.map((error) => (
+                    <li key={error}>{error}</li>
+                  ))}
+                </ul>
+              </div>
+            ) : null}
+
+            {success !== null ? (
+              <div
+                style={{
+                  color: "green",
+                  padding: "0.1rem",
+                  margin: "1rem 0",
+                }}
+              >
+                {success}
+              </div>
+            ) : null}
+
         <h1>SIGN UP</h1>
+        <img src={IMG} alt="Logo Image" />
         </div>
 
         <div className="signup">
@@ -25,14 +106,23 @@ const Signup = ( ) => {
           </NavLink>
         </div>
       <div className="form">
-      <form>
-        <input type="text" name="firstname" placeholder="Enter your firstname................" required   />
-        <input type="text" name="lastname" placeholder="Enter your lastname................" required   />
-        <input type="email" name="email" placeholder="Enter you email............" required  />
-        <input type="text" name="country" placeholder="Kenya..........." required  />
-        <input type="tel" name="phone" placeholder="+254 123 456 789........." required  />
-        <input type="password" name="password" placeholder="Enter you password............" required   />
-        <input type="confirmpassword" name="confirmpassword" placeholder="Confirm password............" required   />
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="firstname" placeholder="Enter your firstname................" required  onChange={handleChange}
+        value={data.firstname}  />
+        <input type="text" name="lastname" placeholder="Enter your lastname................" required   onChange={handleChange}
+        value={data.lastname} />
+        <input type="text" name="fullname" placeholder="Enter your fullname................" required   onChange={handleChange}
+        value={data.fullname} />
+        <input type="email" name="email" placeholder="Enter you email............" required   onChange={handleChange}
+        value={data.email}/>
+        <input type="text" name="country" placeholder="Kenya..........." required   onChange={handleChange}
+        value={data.country}/>
+        <input type="tel" name="phone" placeholder="+254 123 456 789........." required   onChange={handleChange}
+        value={data.phone}/>
+        <input type="password" name="password" placeholder="Enter you password............" required   onChange={handleChange}
+        value={data.password} />
+        <input type="confirmpassword" name="confirmpassword" placeholder="Confirm password............" required   onChange={handleChange}
+        value={data.confirmpassword} />
 
           </form>
         </div>
@@ -41,6 +131,12 @@ const Signup = ( ) => {
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+      </div>
+      <div className="par">
+      <p>“Do not look the other way;
+      do not hesitate. Recognise
+      that the world is hungry for
+       action, not words. Act with courage and vision.”</p>
       </div>
         </div>
     )
