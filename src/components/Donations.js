a5422c383cc80e60518414d637807477848892da
Melyn Atieno Were <melynatieno@gmail.com>
2022-11-01 20:32:46 +0300

    add router file

--- src/components/Donations.js
+++ src/components/Donations.js
@@ -3,33 +3,42 @@ import "./Donations.css";
 import education from "./images/education.jpg";
 import food from './images/food.jpg';
 import sick from "./images/sick.jpg";
+import DonationsForm from "./DonationsForm";
 
 
 
-const Donations = () => {
+function Donations() {
+
   
 
   return (
     
     <div className="donations">
+
       <div className="heading">
       <h2>Donations</h2>
       <span className='line'></span>
       </div>
+
       <div className="btn-donations">
+
       <div className="education">
           <img src={education} alt="education"/>
          <button className="btn-educate"><b>EDUCATE A CHILD</b></button>
       </div>
+
       <div>
         <img src={food} alt="food"/>
         <button className="btn-feed"><b>FEED A FAMILY</b></button>
       </div>
+
       <div>
         <img width={500} src={sick} alt="sick"/>
         <button className="health"><b>HEALTHCARE DONATIONS</b></button>
       </div>
+
       </div>
+      
     </div>
     
   );
--- src/components/DonationsForm.js
+++ src/components/DonationsForm.js
@@ -2,24 +2,29 @@ import React from "react";
 import "./DonationsForm.css"
 
 function DonationsForm(){
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log('Thank you for your donation!');
+      }
+
     return(
         <div className="donation-form-body">
             <h2>DONATE</h2>
             <div className="donation-form">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label><b>DONATION NAME:</b></label>
-                    <input placeholder="Education/Food/Healthcare"></input>
+                    <input placeholder="Education/Food/Healthcare" type="text"></input>
 
                     <label><b>DESCRIPTION:</b></label>
-                    <input placeholder="Describe your donation"></input>
+                    <input placeholder="Describe your donation" type="text"></input>
 
                     <label><b>DONATION AMOUNT:</b></label>
-                    <input placeholder="How much money would you like to contribute?"></input>
+                    <input placeholder="How much money would you like to contribute?" type="text"></input>
 
                     <label><b>COUNTRY CODE:</b></label>
-                    <input placeholder="Enter the country code of the recipient country"></input>
+                    <input placeholder="Enter the country code of the beneficiary" type="text"></input>
 
-                    <button className="form-btn">SEND DONATION</button>
+                    <button className="form-btn" type="submit">SEND DONATION</button>
 
                 </form>
             </div>
