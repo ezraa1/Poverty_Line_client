ba63b979ba2df75aa886c5e50f357ace8732de6e
Melyn Atieno Were <melynatieno@gmail.com>
2022-11-01 19:38:33 +0300

    add css file for donation form

--- src/components/DonationsForm.js
+++ src/components/DonationsForm.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./DonationsForm.css"
 
 function DonationsForm(){
     return(
-        <div className="donation-form">
+        <div className="donation-form-body">
+            <h2>DONATE</h2>
+            <div className="donation-form">
+                <form>
+                    <label><b>DONATION NAME:</b></label>
+                    <input placeholder="Education/Food/Healthcare"></input>
 
+                    <label><b>DESCRIPTION:</b></label>
+                    <input placeholder="Describe your donation"></input>
+
+                    <label><b>DONATION AMOUNT:</b></label>
+                    <input placeholder="How much money would you like to contribute?"></input>
+
+                    <label><b>COUNTRY CODE:</b></label>
+                    <input placeholder="Enter the country code of the recipient country"></input>
+
+                </form>
+            </div>
         </div>
     )
 }
