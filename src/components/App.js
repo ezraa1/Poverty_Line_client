d5bd223a17e75e32640d2e0df4491632a5813fc8
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-11-03 11:37:15 +0300

    feature changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as  Router, Route, Routes } from "react-router-dom";
-import Login from "./Login/login";
-import Signup from "./Signup/signup";
-import Home from "./pages/Home";
 import Dashboard from "./Dashboard/dashboard";
 import Form from "./Form/form"
 import Donation from "./Donation/donation";
@@ -25,21 +22,8 @@ const App = ( ) => {
         <div className="App">
 
             <Routes>
-              <Route
-                path="/"
-                index
-                element={<Home />}
-              />
 
-              <Route
-              path="/signup"
-              element={<Signup />}
-            />
 
-              <Route
-                path="/login"
-                element={<Login />}
-              />
 
               <Route
               path="/dashboard"
