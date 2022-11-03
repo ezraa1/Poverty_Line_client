8b807c21812aeead01750c7bbcdc3e4c6107d1d8
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-11-04 01:28:10 +0300

    done with styling

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Donations from "./components/Donations";
 import Subscribe from "./components/Subscribe";
 import About from "./components/About";
 import Contact from "./components/Contact"
+import Admin from "./components/Admin/admin"
 
 const App = ( ) => {
   return(
@@ -33,6 +34,7 @@ const App = ( ) => {
               <Route  path="/about"  element={<About/>}  />
               <Route  path="/login"  element={<Login/>}  />
               <Route  path="/contact" element={<Contact />} />
+              <Route  path="/admin" element={<Admin />} />
 
         </Routes>
 
