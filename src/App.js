87e7f1c295c27c7d48f3f74d067526a139bad217
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-11-03 12:08:25 +0300

    feature changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard/dashboard";
 import Form from "./components/Form/form"
